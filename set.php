37adadaead4751fe13738cc657f8b9f74b8c60e6
Sepehr Mohammadi <thesepehrm@users.noreply.github.com>
2016-05-29 17:09:42 +0430

    Create set.php

---
+++ set.php
@@ -0,0 +1,19 @@
+<?php
+// Load composer
+require __DIR__ . '/vendor/autoload.php';
+
+$API_KEY = '222942975:AAGV3w63SwXLZMoFa6rKnkYVrP5cICcKTzA';
+$BOT_NAME = 'encrypbot';
+$hook_url = 'https://encbot.herokuapp.com/hook.php';
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
+
+    // Set webhook
+    $result = $telegram->setWebHook($hook_url);
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    echo $e;
+}
