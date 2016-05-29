efdba5511c0248edba784cd8cbbffd405bd627a6
Sepehr Mohammadi <thesepehrm@users.noreply.github.com>
2016-05-29 17:21:23 +0430

    Update hook.php

--- hook.php
+++ hook.php
@@ -4,10 +4,13 @@ require __DIR__ . '/vendor/autoload.php';
 
 $API_KEY = '222942975:AAGV3w63SwXLZMoFa6rKnkYVrP5cICcKTzA';
 $BOT_NAME = 'encrypbot';
+$commands_path = __DIR__ . '/Commands/';
+
 try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
-
+    $telegram->addCommandsPath($commands_path);
+    
     // Handle telegram webhook request
     $telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
